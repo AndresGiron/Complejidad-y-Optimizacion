1d7fca990bf874d8cb037f88c5835a0399fabf94
Andres Felipe Giron Perez <gyron@pop-os.localdomain>
2022-11-27 17:28:55 -0500

    Arreglando minizinc function

---
+++ PeriodicoGUI-Fuentes/Minizinc.py
@@ -0,0 +1,49 @@
+import subprocess
+import minizinc
+import pymzn
+def minizinzFunction(values):
+    cat = int(values['cantidad'])
+    maxp = values['PagsArticulo']
+    pagMin = []
+    pagMax = []
+    rdrs = []
+    for i in range(0,cat):
+        pagMin.append(int(values['min'+str(i)]))
+    for i in range(0,cat):
+        pagMax.append(int(values['max'+str(i)]))
+    for i in range(0,cat):
+        rdrs.append(int(values['readers'+str(i)]))
+
+    #Creando el diccionario con los parametros 
+    dict = {}
+    dict['cat'] = cat
+    dict['maxp'] = maxp
+    dict['pagMin'] = pagMin
+    dict['pagMax'] = pagMax
+    dict['rdrs'] = rdrs
+
+    #Convirtiendolo a dzn
+    pymzn.dict2dzn(dict, fout="../datos.dzn")
+
+    #Ubicacion del modelo y ubicacion de parametros
+    folder = minizinc.Model("../ModeloProyecto.mzn")
+    folder.add_file("../datos.dzn")
+    
+
+    chuffed = minizinc.Solver.lookup("gecode")
+    instance = minizinc.Instance(chuffed, folder)
+
+    result = instance.solve()
+    print(result)
+
+
+
+    # dzn = 'cat = %s; \nmaxp = %s; \npagMin = %s; \npagMax = %s; \nrdrs = %s; \n'%(str(cat),maxp,str(pagMin),str(pagMax),str(rdrs))
+    
+    # with open("../datos.dzn","w") as file:
+    #     file.write(dzn)
+
+
+
+values = {'cantidad': '5', 'tipo0': '1', 'min0': '5', 'max0': '9', 'readers0': '1500', 'tipo1': '23', 'min1': '4', 'max1': '7', 'readers1': '2000', 'tipo2': '3', 'min2': '2', 'max2': '5', 'readers2': '1000', 'tipo3': '4', 'min3': '2', 'max3': '4', 'readers3': '1500', 'tipo4': '5', 'min4': '1', 'max4': '3', 'readers4': '750', 'PagsArticulo': '10'}
+minizinzFunction(values)
